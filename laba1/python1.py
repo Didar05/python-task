74bb39229c800dc32dd38d7b524a44b10ec2ac0a
Denis4002 <101782555+Denis4002@users.noreply.github.com>
2022-03-17 12:58:20 +0600

    Add files via upload

---
+++ laba1/python1.py
@@ -0,0 +1,13 @@
+x=5
+y=6
+print('y = ',x)
+
+print('y = ',y)
+z=x+y
+print('z = ',z)
+z=x-y
+print('z =',z)
+z=x*y
+print('z = ',z)
+z=x/y 
+print('z = ',z)
---
+++ laba1/python6.py
@@ -0,0 +1,4 @@
+name = input("Ваша фамилия?")
+place = input("Где вы живете?")
+print("Ваша фамилия",name)
+print("Вы живете в",place)
